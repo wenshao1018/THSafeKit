b84e982a0a6d212e1406d25b8220b1785e343c12
WenQing <liuwenqing2@rainbowcn.com>
2017-07-05 09:51:54 +0800

    增加unrecognize selector崩溃处理

--- THSafeKit/FoundationKit/ARC/NSObject+THSafeKit.m
+++ THSafeKit/FoundationKit/ARC/NSObject+THSafeKit.m
@@ -8,6 +8,13 @@
 
 #import "NSObject+THSafeKit.h"
 #import "NSObject+THRuntime.h"
+#import <objc/runtime.h>
+
+//防止崩溃函数
+void dynamicMethodIMP(id self,SEL _cmd)
+{
+    
+}
 
 @implementation NSObject (THSafeKit)
 
@@ -15,6 +22,8 @@
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
+        [self exchangeImplementationsWithClass:NSStringFromClass(self) fromMethodSelector:@selector(methodSignatureForSelector:) toMethodSelector:@selector(safe_methodSignatureForSelector:) isInstanceMethod:YES];
+        [self exchangeImplementationsWithClass:NSStringFromClass(self) fromMethodSelector:@selector(forwardInvocation:) toMethodSelector:@selector(safe_forwardInvocation:) isInstanceMethod:YES];
         [self exchangeImplementationsWithClass:NSStringFromClass(self) fromMethodSelector:@selector(setValue:forKey:) toMethodSelector:@selector(safe_setValue:forKey:) isInstanceMethod:YES];
         [self exchangeImplementationsWithClass:NSStringFromClass(self) fromMethodSelector:@selector(setValue:forKeyPath:) toMethodSelector:@selector(safe_setValue:forKeyPath:) isInstanceMethod:YES];
         [self exchangeImplementationsWithClass:NSStringFromClass(self) fromMethodSelector:@selector(setValue:forUndefinedKey:) toMethodSelector:@selector(safe_setValue:forUndefinedKey:) isInstanceMethod:YES];
@@ -22,6 +31,32 @@
         [self exchangeImplementationsWithClass:NSStringFromClass(self) fromMethodSelector:@selector(removeObserver:forKeyPath:) toMethodSelector:@selector(safe_removeObserver:forKeyPath:) isInstanceMethod:YES];
         [self exchangeImplementationsWithClass:NSStringFromClass(self) fromMethodSelector:@selector(removeObserver:forKeyPath:context:) toMethodSelector:@selector(safe_removeObserver:forKeyPath:context:) isInstanceMethod:YES];
     });
+}
+
+- (NSMethodSignature *)safe_methodSignatureForSelector:(SEL)aSelector
+{
+    if (![self respondsToSelector:aSelector]) {
+        NSMethodSignature *methodSignature = [self safe_methodSignatureForSelector:aSelector];
+        class_addMethod([self class], aSelector, (IMP)dynamicMethodIMP, "v@:");
+        if (!methodSignature) {
+            methodSignature = [self safe_methodSignatureForSelector:aSelector];
+        }
+        
+        return methodSignature;
+        
+    }else{
+        return [self safe_methodSignatureForSelector:aSelector];
+    }
+}
+
+- (void)safe_forwardInvocation:(NSInvocation *)anInvocation
+{
+    SEL selector = [anInvocation selector];
+    if ([self respondsToSelector:selector]) {
+        [anInvocation invokeWithTarget:self];
+    }else{
+        [self safe_forwardInvocation:anInvocation];
+    }
 }
 
 - (void)safe_removeObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath
